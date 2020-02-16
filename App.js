724d15a9fee2bd038398d7d5470e3ba61eb364c7
Blazej Kowalczyk <bikowalczyk@gmail.com>
2020-02-16 14:49:59 +0100

    input container styled

--- App.js
+++ App.js
@@ -1,59 +1,20 @@
-import React, { useState } from "react";
-import { StyleSheet, View, FlatList, Button } from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 
-import GoalItem from "./components/GoalItem";
-import GoalInput from "./components/GoalInput";
+import Header from "./components/Header";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState("");
-  const [courseGoals, setCourseGoals] = useState([]);
-  const [isAddMode, setIsAddMode] = useState(false);
-
-  const goalInputHandler = value => {
-    setEnteredGoal(value);
-  };
-
-  const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [
-      ...courseGoals,
-      { id: Math.random().toString(), value: enteredGoal }
-    ]);
-    setIsAddMode(false);
-  };
-
-  const removeGoalHandler = goalId => {
-    setCourseGoals(currentGoals => {
-      return currentGoals.filter(goal => goal.id !== goalId);
-    });
-  };
-
   return (
     <View style={styles.screen}>
-      <Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
-      <GoalInput
-        visible={isAddMode}
-        setIsAddMode={setIsAddMode}
-        goalInputHandler={goalInputHandler}
-        enteredGoal={enteredGoal}
-        setEnteredGoal={setEnteredGoal}
-        addGoalHandler={addGoalHandler}
-      />
-      <FlatList
-        data={courseGoals}
-        renderItem={itemData => (
-          <GoalItem
-            title={itemData.item.value}
-            id={itemData.item.id}
-            onDelete={removeGoalHandler}
-          />
-        )}
-      ></FlatList>
+      <Header title={"Guess a number"} />
+      <StartGameScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 50
+    flex: 1
   }
 });
